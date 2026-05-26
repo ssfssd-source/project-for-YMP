dbc48b49922bb6e5224f5f11a84068d6c7d8a889
В-302 <v302@corp.knrtu.ru>
2026-05-26 11:37:36 +0300

    added basic (de)coder logic

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 "Проект для 'ЯМП'а: компактный кодировщик шифра цезаря"
+
+ru_alphabet ="абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+def ceasar_ciper(text:str, shift:int):
+    "Шифрование и расшифрование(для дешифровки передаем открицательный shift)"
+    res=[]
+    for c in text:
+        if c.lower() in ru_alphabet:
+            idx = (ru_alphabet.index(c.lower()) + shift) % 33
+            res.append(ru_alphabet[idx])
+        else:
+            res.append(c)
+    return "".join(res)
+
+print(ceasar_ciper("воздухан", 5))
